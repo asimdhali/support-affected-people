84c421f5e8fb16779290dd7c3b315acf5806df5c
1asimdhali <s18237018448@nu.ac.bd>
2024-09-23 13:42:41 +0600

    bug fixes for Donate Now Button, Donated TK & Main TK

--- js/donateNoakhali.js
+++ js/donateNoakhali.js
@@ -1,17 +1,31 @@
 document.getElementById('donateBtnNk').addEventListener('click', function(event){
-    const nkInputAmount = document.getElementById('nkInput').value;
+    
+    const nkInput = document.getElementById('nkInput');
     const nkDonatedId = document.getElementById('nkMainTk');
-    const nkDonatedTk = parseFloat(nkDonatedId.innerText);
-    
-    const nkNewTk = parseFloat(nkInputAmount) + nkDonatedTk;
-    nkDonatedId.innerText = nkNewTk;
-    
-    // MAIN TK
     const mainTk = document.getElementById('main-tk');
-    const mainTkUpdate =  parseFloat(mainTk.innerText) - nkInputAmount;
-    mainTk.innerText = mainTkUpdate;
 
+    const nkInputAmount = parseFloat(nkInput.value)
+    const nkDonatedTk = parseFloat(nkDonatedId.innerText);
+    const mainTkNum = parseFloat(mainTk.innerText)
 
-    // HISTORY UPDATE 
-    showhistoryUpdate(nkInputAmount, 'Flood Relief in Noakhali Bangladesh');
+    // NOT NEGATIVE, NaN AND NULL
+    if(nkInputAmount <= 0 || nkInputAmount === '' || isNaN(nkInputAmount)){
+        alert('Invalid Donation Amount');
+        return
+    } else if(mainTkNum <= 0 || nkInputAmount > mainTkNum){
+        alert('Please check your main balance')
+        return
+    } else{
+        const nkNewTk = nkInputAmount + nkDonatedTk;
+        nkDonatedId.innerText = nkNewTk;
+        
+        // MAIN TK
+        const mainTkUpdate =  mainTkNum - nkInputAmount;
+        mainTk.innerText = mainTkUpdate;
+
+        // HISTORY UPDATE 
+        showhistoryUpdate(nkInputAmount, 'Flood Relief in Noakhali Bangladesh');
+
+        document.getElementById('nkInput').value = '';
+    }
 })
