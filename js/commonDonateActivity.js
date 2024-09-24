4b154ffd8768bd9bbe6a425d4e5d9f3aa1f3fe17
1asimdhali <s18237018448@nu.ac.bd>
2024-09-24 10:48:31 +0600

    make full Responsive & correction of different section

--- js/commonDonateActivity.js
+++ js/commonDonateActivity.js
@@ -1,11 +1,14 @@
+// NOAKHALI SECTION
 document.getElementById('donateBtnNk').addEventListener('click', function(){
     donateNowAllEffect('nkInput', 'nkMainTk', 'Flood at Noakhali, Bangladesh');
 });
 
+// FENI SECTION
 document.getElementById('doanteBtnFeni').addEventListener('click', function(event){
     donateNowAllEffect('feniInput', 'feniMainTk', 'Flood Relief in Feni');
 });
 
+// QUOTA SECTION
 document.getElementById('donateBtnQta').addEventListener('click', function(event){
     donateNowAllEffect('qtaInput', 'qtaMainTk', 'Injured in the Quota Movement');
 });
